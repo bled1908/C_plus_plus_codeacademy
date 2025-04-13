e5f7089bd98b4970d34d1ba529abf6a97480648d
bled1908 <bled19082005@gmail.com>
2025-04-13 20:58:00 +0530

    letter

---
+++ letter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Goal: Earn more than 20 LPA in 2025.\n";
+  std::cout << "Name - Subhadeep Roy.\n";
+  std::cout << "Date - 13-04-2025.\n";
+  
+  
+}
